8b560d855eeae0f4884c8578c6810fccead4ee40
Fabian <fabrue@gmail.com>
2021-03-20 09:58:04 +0100

    introduce string converter

---
+++ modules/string-inverter/iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "lambda_execution" {
+  name = "string_inverter_execution_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+inline_policy {
+    name = "string_inverter_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["s3:*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
---
+++ modules/string-inverter/lambda-string-inverter/string-inverter.js
@@ -0,0 +1,3 @@
+exports.handler = async (_, context) => {
+    console.log(`string inverter`);
+}
