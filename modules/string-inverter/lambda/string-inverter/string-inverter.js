e6a48727b95b2a75a3879673b39180f63b5ae452
Fabian <fabrue@gmail.com>
2021-03-20 10:58:07 +0100

    add sample s3-select config

--- modules/string-inverter/lambda/string-inverter/string-inverter.js
+++ modules/string-inverter/lambda/string-inverter/string-inverter.js
@@ -1,3 +1,35 @@
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+const params = {
+    Bucket: 'dd-challenge-application-storage',
+    Expression: 'SELECT * FROM s3object',
+    ExpressionType: 'SQL',
+    InputSerialization: {
+        JSON: {
+            Type: 'DOCUMENT',
+          }
+    },
+    Key: 'words.csv',
+    OutputSerialization: {
+        JSON: {
+            RecordDelimiter: ','
+          }
+    }
+}
+
+const fetchStrings = async () => {
+    return new Promise((resolve, reject) => {
+        S3.selectObject
+    })
+}
+
 exports.handler = async (_, context) => {
     console.log(`string inverter`);
+
+    try{
+
+    } catch(e) {
+
+    }
 }
