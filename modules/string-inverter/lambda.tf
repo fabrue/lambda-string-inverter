ec9e6dccc3b672fcc9fbbf1ed67ed623544caa06
Fabian <fabrue@gmail.com>
2021-03-20 10:23:20 +0100

    refactor: interpolation expression

--- modules/string-inverter/lambda.tf
+++ modules/string-inverter/lambda.tf
@@ -5,8 +5,8 @@ data "archive_file" "zip_string_inverter_lambda" {
 }
 
 resource "aws_lambda_function" "string_inverter" {
-  filename         = "${data.archive_file.zip_string_inverter_lambda.output_path}"
-  source_code_hash = "${data.archive_file.zip_string_inverter_lambda.output_base64sha256}"
+  filename         = data.archive_file.zip_string_inverter_lambda.output_path
+  source_code_hash = data.archive_file.zip_string_inverter_lambda.output_base64sha256
   role          = aws_iam_role.lambda_execution.arn
   handler       = "node-function.handler"
   function_name = "string-inverter"
