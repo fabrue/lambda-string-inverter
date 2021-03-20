401510216c1587607c1bb19a31b62b5be8db03f1
Fabian <fabrue@gmail.com>
2021-03-20 10:26:10 +0100

    refactor: seperate s3 in seperate file

---
+++ modules/string-inverter/application-storage.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "application_storage" {
+  bucket = "dd-challenge-application-storage"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+    }
+  }
+}
--- modules/string-inverter/main.tf
+++ modules/string-inverter/main.tf
@@ -9,18 +9,3 @@ terraform {
     region = "eu-central-1"
   }
 }
-
-resource "aws_s3_bucket" "application_storage" {
-  bucket = "dd-challenge-application-storage"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
-    }
-  }
-}
