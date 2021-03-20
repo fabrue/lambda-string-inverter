c332acc2d4d18c3bdcf19c52c52b34403cc40067
Fabian <fabrue@gmail.com>
2021-03-20 10:17:47 +0100

    add s3 backend

--- modules/string-inverter/main.tf
+++ modules/string-inverter/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "dd-challenge"
+    key    = "state"
+    region = "eu-central-1"
+  }
+}
+
 resource "aws_s3_bucket" "application_storage" {
   bucket = "dd-challenge-application-storage"
   acl = "private"
