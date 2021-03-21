c7c770b9d88779f8fc2ac571202ec4809093eceb
Fabian <fabrue@gmail.com>
2021-03-21 19:14:20 +0100

    extract and fetch words

--- modules/string-inverter/lambda/string-inverter/string-inverter.js
+++ modules/string-inverter/lambda/string-inverter/string-inverter.js
@@ -1,5 +1,5 @@
-const AWS = require('aws-sdk');
-const S3 = new AWS.S3();
+const awssdk = require('aws-sdk');
+const S3 = new awssdk.S3();
 
 const params = {
     Bucket: 'dd-challenge-application-storage',
@@ -18,28 +18,58 @@ const params = {
     }
 }
 
-const fetchStrings = async () => {
+const wordsToArray = async (words) => {
+   let returnArr = [];
+   
+   const extractWord = (input) => {
+       // Input row's structure is { "_1":"word"}, this function extracts the word
+       return JSON.parse(input)._1;
+   }
+   
+   const wordArr = words.split(','); //?
+   wordArr.forEach((word, i) => {
+       console.log(`${i} word is ${word}`);
+
+       if(word) {
+           console.log(`${i} word is true`)
+           const parsedWord = JSON.parse(word)._1 //?
+           const functionParsedWord = extractWord(word); //?
+           returnArr.push(functionParsedWord); //? 
+        }
+    });
+    return returnArr;
+}
+
+const fetchWords = async (params) => {
+    if (!params) { throw new Error('No params provided'); }
+
     return new Promise((resolve, reject) => {
         S3.selectObjectContent(params, (err, data) => {
-            if(err) {
-                console.error(err, err.stack);
+            if (err) { reject(err); }
+
+            let words = [];
+
+            data.Payload.on('data', (event) => {
+                if(event.Records) { words.push(event.Records.Payload); }
+            })
+            .on('error', (err) => {
                 reject(err);
-            }
-            else {
-                console.log(data);
-                resolve(data);
-            }
-        })
+            })
+            .on('end', (event) => {
+                resolve(wordsToArray(Buffer.concat(words).toString()));
+            })
+        });
     })
 }
 
-exports.handler = async (_, context) => {
+exports.handler = async (event, context) => {
     console.log(`string inverter`);
 
     try{
-        const words = await fetchStrings();
-        console.log(`Payload ${JSON.stringify(words.Payload)}`);
+        const words = await fetchWords(params);
+        console.log(words);
     } catch(e) {
         console.error('ERROR');
+        console.log(e.message);
     }
 }
